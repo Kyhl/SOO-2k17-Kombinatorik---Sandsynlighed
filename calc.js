ed3f7439e19d5cd3d1e4f2f1824808bc15d0a678
Kyhl <kasper.sebastian@gmail.com>
2017-06-06 13:24:28 +0200

    Done

--- calc.js
+++ calc.js
@@ -1,22 +1,31 @@
 function onLoad() {
-	//Recursive();
 	Iterative();
 }
 
+// Min funktion ved navn "Iterative" kaldes.
 function Iterative(num){
+
+	//Variablet "i" defineres
 	var i;
-	var num;
+
+	//Parsefloat omsætter stringen der bliver modtaget fra HTML-koden til et nummer
 	num=parseFloat(document.index.num.value);
+	
+	// De resterende variabler defineres
+	var num;
 	var res1=1;
-	if(num == undefined || num == null){
-		num = Math.ceil(Math.random()*10);
-	}
+
+	//Sætter værdien til 1 hvis tallet er 0 eller 1, da disse tal ikke er gyldige i formlen.
 	if(num == 0 || num == 1){
 		res1 = 1;
-	}else{
+	}
+
+	//else funktion, der sikrer at det indtastede nummer er over 1, og eksekverer fakultetudregningen.
+	else{
 		for(i=1; i<=num; i++){
 			res1 = res1*i;
 		}
 	}
+	//Printfunktion, der sender fakultetværdien tilbage, og viser den på skærmen.
 	document.index.resultat1.value=res1;
 }
